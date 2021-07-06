63813ab5577f18bd76275da03394540e068ef408
Daisy Chen <qoo8857300@gmail.com>
2021-07-06 21:19:27 +0800

    Add page - Login & Regist

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
+import Login from '../views/Login.vue'
 
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
+    path: '/users',
+    name: 'regist',
+    component: () => import('../views/Regist.vue')
+  },
+  {
+    path: '/users/:id',
+    name: 'account-setting',
+    component: () => import('../views/AccountSetting.vue')
+  },
   {
     path: '*',
     name: 'not-found',
