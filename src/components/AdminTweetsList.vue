cf0fcc8adac34ba26a8fcf2bc665bc8b24a4e412
RyanHsun <lenxun.huang@gmail.com>
2021-07-08 01:04:27 +0800

    feat: add components - AdminTweetsList.vue & image assets

---
+++ src/components/AdminTweetsList.vue
@@ -0,0 +1,201 @@
+<template>
+  <ul class="tweets-list">
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+      </div>
+      <button class="delete-tweet" type="button">
+        <img src="~@/assets/img/icon_close.svg" alt="">
+      </button>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+      </div>
+      <button class="delete-tweet" type="button">
+        <img src="~@/assets/img/icon_close.svg" alt="">
+      </button>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+      </div>
+      <button class="delete-tweet" type="button">
+        <img src="~@/assets/img/icon_close.svg" alt="">
+      </button>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+      </div>
+      <button class="delete-tweet" type="button">
+        <img src="~@/assets/img/icon_close.svg" alt="">
+      </button>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+      </div>
+      <button class="delete-tweet" type="button">
+        <img src="~@/assets/img/icon_close.svg" alt="">
+      </button>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+      </div>
+      <button class="delete-tweet" type="button">
+        <img src="~@/assets/img/icon_close.svg" alt="">
+      </button>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+      </div>
+      <button class="delete-tweet" type="button">
+        <img src="~@/assets/img/icon_close.svg" alt="">
+      </button>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+      </div>
+      <button class="delete-tweet" type="button">
+        <img src="~@/assets/img/icon_close.svg" alt="">
+      </button>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+      </div>
+      <button class="delete-tweet" type="button">
+        <img src="~@/assets/img/icon_close.svg" alt="">
+      </button>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.tweet {
+  border-bottom: 1px solid #e6ecf0;
+  display: flex;
+  align-items: flex-start;
+  padding: 10px 15px;
+  width: 100%;
+}
+.tweet-info {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: calc( 100% - 60px);
+  text-align: left;
+}
+.name {
+  margin-right: 10px;
+  font-weight: bold;
+}
+.tweet-content {
+  margin: 10px 0;
+}
+.tweet-panel span {
+  padding-right: 40px;
+}
+.tweet-panel img{
+  margin-right: 20px;
+}
+.delete-tweet {
+  margin-top: 5px;
+  border: none;
+  background: none;
+}
+</style>
