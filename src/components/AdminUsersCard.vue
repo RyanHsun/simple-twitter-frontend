e5429fad9b63040d7e0b18dbfc49d6403061fd9a
RyanHsun <lenxun.huang@gmail.com>
2021-07-08 01:05:37 +0800

    feat: add components - AdminUsersCard.vue

---
+++ src/components/AdminUsersCard.vue
@@ -0,0 +1,366 @@
+<template>
+  <ul class="admin-users-list">
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+    <li class="admin-user">
+      <div class="user-profile-head">
+        <div class="user-cover">
+          <img src="~@/assets/img/cover_photo.jpg" alt="">
+        </div>
+        <a class="user-avatar avatar">
+          <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+        </a>
+      </div>
+      <a href="" class="user-info">
+        <span class="name">user1</span>
+        <span class="account">@user1</span>
+      </a>
+      <div class="user-panel">
+        <span class="comment">
+          <img src="~@/assets/img/icon_comment.svg" alt="">1
+        </span>
+        <span class="likes">
+          <img src="~@/assets/img/icon_like.svg" alt="">2
+        </span>
+      </div>
+      <div class="user-followships">
+        <div class="user-following">34 個跟隨中</div>
+        <div class="user-follower">808 位跟隨者</div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+  .admin-users-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    padding: 10px;
+  }
+  .admin-user {
+    max-width: 235px;
+    margin: 0 10px 10px 0;
+    padding-bottom: 10px;
+    text-align: center;
+    border-radius: 10px;
+    background-color: #F6F7F8;
+    overflow: hidden;
+  }
+  .admin-user > div {
+    display: flex;
+    justify-content: center;
+  }
+  .user-profile-head {
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+  .user-cover {
+    width: 100%;
+    margin-bottom: 10px;
+  }
+  .user-cover img {
+    width: 100%;
+    height: 140px;
+    object-fit: cover;
+  }
+  .user-avatar {
+    width: 100px;
+    height: 100px;
+    margin: -86px auto 0;
+  }
+  .user-avatar img {
+    border: 5px solid #fff;
+  }
+  .user-edit {
+    margin-right: 20px;
+  }
+  .user-info {
+    display: inline-block;
+    padding: 0 20px;
+  }
+  .user-info span{
+    display: block;
+  }
+  .name {
+    font-size: 19px;
+  } 
+  .admin-user .user-panel{
+    width: 100px;
+    margin: auto;
+    justify-content: space-around;
+  }
+  .user-panel img {
+    width: 15px;
+    height: 15px;
+    margin-right: 6px;
+  }
+  .user-followships {
+    padding: 10px 20px;
+  }
+  .user-followships {
+    font-size: 14px;
+  }
+  .user-following {
+    margin-right: 20px;
+  }
+</style>
