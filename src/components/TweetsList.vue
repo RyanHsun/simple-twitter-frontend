87402b675acc8f68f4b4a087d3fc006ebb08a3cf
RyanHsun <lenxun.huang@gmail.com>
2021-07-08 00:53:31 +0800

    feat: add components - TweetsList.vue

---
+++ src/components/TweetsList.vue
@@ -0,0 +1,194 @@
+<template>
+  <ul class="tweets-list">
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+        <div class="tweet-panel">
+          <span class="comment">
+            <img src="~@/assets/img/icon_comment.svg" alt="">1
+          </span>
+          <span class="likes">
+            <img src="~@/assets/img/icon_like.svg" alt="">2
+          </span>
+        </div>
+      </div>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+        <div class="tweet-panel">
+          <span class="comment">
+            <img src="~@/assets/img/icon_comment.svg" alt="">1
+          </span>
+          <span class="likes">
+            <img src="~@/assets/img/icon_like.svg" alt="">2
+          </span>
+        </div>
+      </div>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+        <div class="tweet-panel">
+          <span class="comment">
+            <img src="~@/assets/img/icon_comment.svg" alt="">1
+          </span>
+          <span class="likes">
+            <img src="~@/assets/img/icon_like.svg" alt="">2
+          </span>
+        </div>
+      </div>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+        <div class="tweet-panel">
+          <span class="comment">
+            <img src="~@/assets/img/icon_comment.svg" alt="">1
+          </span>
+          <span class="likes">
+            <img src="~@/assets/img/icon_like.svg" alt="">2
+          </span>
+        </div>
+      </div>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+        <div class="tweet-panel">
+          <span class="comment">
+            <img src="~@/assets/img/icon_comment.svg" alt="">1
+          </span>
+          <span class="likes">
+            <img src="~@/assets/img/icon_like.svg" alt="">2
+          </span>
+        </div>
+      </div>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+        <div class="tweet-panel">
+          <span class="comment">
+            <img src="~@/assets/img/icon_comment.svg" alt="">1
+          </span>
+          <span class="likes">
+            <img src="~@/assets/img/icon_like.svg" alt="">2
+          </span>
+        </div>
+      </div>
+    </li>
+    <li class="tweet">
+      <a href="" class="avatar">
+        <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="">
+      </a>
+      <div class="tweet-info">
+        <div class="user-info">
+          <a class="name" href="">user1</a>
+          <span class="account">@user1</span>
+          <span class="tweet-update-at">・3小時</span>
+        </div>
+        <div class="tweet-content">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+        </div>
+        <div class="tweet-panel">
+          <span class="comment">
+            <img src="~@/assets/img/icon_comment.svg" alt="">1
+          </span>
+          <span class="likes">
+            <img src="~@/assets/img/icon_like.svg" alt="">2
+          </span>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.tweet {
+  border-bottom: 1px solid #e6ecf0;
+  display: flex;
+  padding: 10px 15px;
+  width: 100%;
+}
+.tweet-info {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: calc( 100% - 60px);
+  text-align: left;
+}
+.name {
+  margin-right: 10px;
+  font-weight: bold;
+}
+.tweet-content {
+  margin: 10px 0;
+}
+.tweet-panel span {
+  padding-right: 40px;
+}
+.tweet-panel img{
+  margin-right: 20px;
+}
+</style>
