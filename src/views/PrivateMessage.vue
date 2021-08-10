4bc3ef80878db75f4d030ae10878da42ced76865
RyanHsun <lenxun.huang@gmail.com>
2021-08-10 17:41:55 +0800

    fix: socket function-get_private_rooms in PrivateMessage change on methods to emit use callback function catch data

--- src/views/PrivateMessage.vue
+++ src/views/PrivateMessage.vue
@@ -66,7 +66,7 @@ export default {
     PrivateChatroom,
     // Spinner
   },
-  data () {
+  data() {
     return {
       socket: null,
       userRooms: [],
@@ -75,7 +75,6 @@ export default {
       messages: [],
       unseenNum: 0,
       unreadRooms: [],
-      messageNotice: {},
       isLoading: true
     }
   }, 
@@ -83,19 +82,6 @@ export default {
     ...mapState(['currentUser'])
   },
   sockets: {
-    get_private_rooms(Rooms) {
-      // this.userRooms = Rooms
-      // console.log('取得當前使用者所有私訊聊天室', Rooms)
-
-      this.userRooms = Rooms.map( user => {
-        const { id, lastMsg, roomMember } = user
-        const isLinked = id === this.currentRoom.id ? true : false
-        return { id, lastMsg, roomMember, isLinked }
-      })
-      // console.log('取得當前使用者所有私訊聊天室', this.userRooms)
-
-      this.isLoading = false
-    },
     join_private_room(data) {
       console.log('加入room的data',data)
       // this.privateRoom.push(data)
@@ -134,14 +120,14 @@ export default {
     // userRooms () {
     //   this.afterClick()
     // }
-    privateRoomAwait () {
+    privateRoomAwait() {
       this.currentRoom = this.privateRoomAwait
       const User1Id = this.currentUser.id
       const User2Id = this.currentRoom.userId
       this.join_private_room({User1Id,User2Id})
       this.get_private_history(this.currentRoom.id)
     },
-    unreadRooms () {
+    unreadRooms() {
       // console.log(this.unreadRooms.length)
       for (let i = 0; i < this.unreadRooms.length; i++) {
         // console.log('未讀的聊天室：', this.unreadRooms[i])
@@ -153,21 +139,12 @@ export default {
         })
         // console.log(`第${i}輪聊天室詳細資料：`, this.userRooms)
       }
-    },
-    // messageNotice () {
-    //   this.userRooms = this.userRooms.map( user => {
-    //     if (user.id === this.messageNotice.id) {
-    //       user.unreadNum = this.messageNotice.unreadNum
-    //     }
-    //   })
-    // }
+    }
   },
   created() {
     this.join_private_page(this.currentUser.id)
-    this.$socket.on('get_private_rooms')
     this.catchRoomUserId()
     this.unseenNum = localStorage.getItem('unseenNum')
-    // this.fetchUserRooms()
   },
   updated() {
     this.$socket.on('join_private_room')
@@ -176,7 +153,33 @@ export default {
     join_private_page(userId) { 
       this.$socket.emit('join_private_page', { userId })
       localStorage.removeItem('unseenNum')
-      // console.log(`使用者：${userId} 進入到私人訊息頁面了`)
+      this.get_private_rooms()
+    },
+    get_private_rooms() {
+      this.$socket.emit('get_private_rooms', {
+        offset: 0,
+        limit: 20
+        }, data => {
+          // this.userRooms = data
+          
+          this.userRooms = data.map( user => {
+            const { id, lastMsg, roomMember } = user
+            const isLinked = id === this.currentRoom.id ? true : false
+            return { id, lastMsg, roomMember, isLinked }
+          })
+
+          if (this.unreadRooms.length > 0) {
+            for (let i = 0; i < this.unreadRooms.length; i++) {
+              this.userRooms = this.userRooms.map( user => {
+                const { id, lastMsg, roomMember, isLinked } = user
+                const unreadNum = user.roomMember.id === this.unreadRooms[i].SenderId ? this.unreadRooms[i].unreadNum : user.unreadNum
+                return { id, lastMsg, roomMember, isLinked, unreadNum }
+              })
+            }
+          }
+          // console.log('新的使用者清單：', this.userRooms)
+        }
+      )
     },
     join_private_room({User1Id,User2Id}) { 
 
@@ -186,13 +189,10 @@ export default {
       // console.log('進入與誰的私訊：', User2Id)
       // console.log('私人房號',this.privateRoom)
     },
-    addMsgUser () {
+    addMsgUser() {
       console.log('跳窗顯示所有使用者')
     },
-    afterClick (user) {
-      // this.unseenNum = this.unseenNum - 1
-      // localStorage.setItem('unseenNum', this.unseenNum)
-
+    afterClick(user) {
       this.currentRoom = {
         id: user.id,
         userId: user.roomMember.id,
@@ -207,20 +207,12 @@ export default {
         }
         return user
       })
-      // console.log('所有房間', this.userRooms)
-      // console.log(`開啟的聊天室房號：${this.currentRoom.id}`)
-      // console.log(`開啟的聊天室對方使用者ID：${this.currentRoom.userId}`)
-      this.get_private_history(this.currentRoom.id)
 
-      // const User1Id = this.currentUser.id
-      // const User2Id = this.currentRoom.userId
-      // this.join_private_room({User1Id,User2Id})
       this.privateRoomAwait = this.currentRoom
       localStorage.setItem('privateRoomAwait', JSON.stringify(this.privateRoomAwait))
       this.$router.push({ name: 'message-await', params: { id: this.currentRoom.id } })
     },
     get_private_history(roomId) { 
-      // console.log(roomId)
       this.$socket.emit('get_private_history', {
         offset: 0,
         limit: 20,
@@ -235,9 +227,6 @@ export default {
           return { UserId, avatar, content, createdAt, isSelf }
         })
         this.isLoading = false
-        // console.log('歷史訊息：', this.messages)  
-        // console.log('歷史訊息：', data)  
-        // this.messages.push( data )
       })
     },
     catchRoomUserId () {
